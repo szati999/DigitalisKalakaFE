9d1545df32b29e3e47633b932c80c28a88a07b79
Gyorgy <ujvari.gyorgy@gmail.com>
2021-12-10 21:32:21 +0200

    router setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-     ez az app ugye?
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              {/* <Link to="/mentor">Mentor</Link> */}
+              <a href="/mentor">mentor</a>
+            </li>
+            <li>
+              {/* <Link to="/student">Student</Link> */}
+              <a href="/student">student</a>
+
+            </li>
+            
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/mentor">
+          </Route>
+          <Route path="/student">
+          </Route>
+          <Route path="/">
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default App;
---
+++ src/components/UserTypes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UserTypes() {
+    return (
+        <div>
+            first page
+        </div>
+    )
+}
