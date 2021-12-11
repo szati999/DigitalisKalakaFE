73f6383600b1fc7e4f5fcba34a783f8916837352
Gyorgy <ujvari.gyorgy@gmail.com>
2021-12-11 14:39:03 +0200

    select user page

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,33 +1,55 @@
 import React , {useEffect, useState} from 'react'
 import axios from 'axios';
 import './StudentList.css'
-
+import mentorimage from './mentor.svg'
+import certificate from './certificate.svg'
+import motivation from './motivation.svg'
 
 
 export default function StudentList() {
     const [userList, setUserList] = useState([])
-   
+
     useEffect(() => {
         // Update the document title using the browser API
         axios.get('http://localhost:3000/getAllStudents').then(resp => {
-                console.log(resp.data);
                 setUserList(resp.data)
         });
-        console.log(userList,'asdasd')
       },[]);
-   
     return (
         <div className='student-card-container'>
-            {
-                userList.map((item)=>(
-                    <div className='student-card'>
-                         <div>{item.category}</div>
-                         <div>{item.name}</div>
-                         <div>{item.email}</div>
+            <div className='student-card-holder'>
+                <div className='title-container'>
+                    <div className='title'>Mentorok</div>
+                    <div className='subtitle'>Találd meg a számodra legmegfelelőbb mentort, vedd fel vele a kapcsolatot és máris kezdődhet a korrepetálás.</div>
+                </div>
+                    <div className='holder'>
+                        {
+                            userList.map((item)=>(
+                                <div className='student-card'>
+                                    <div className='profile-image'><img src={mentorimage}/></div>
+                                    <div className='student-name'>{item.name}</div>
+                                    <div className='category'>
+                                        <div><img src={certificate}/></div>
+                                        <div>
+                                            <div className='profil'>Profil</div>
+                                            <div>{item.category}</div>
+                                        </div>
+                                    </div>
+                                        <div className='motivation'>
+                                        <div><img src={motivation}/></div>
+                                            <div>
+                                                <div className='profil'>Rólam</div>
+                                                <div>Lorem ipsum dolor sit amet, cons adipiscing elit. Donec dignissim mi et dui pretium, eget sodales urna lacinia. Orci varius natoque penatibus et magnis dis parturien montes, nascetur ridiculus mus. </div>
+                                            </div>
+                                        </div>
+                                        <div className='button-container'>
+                                             <button className="btn btn-primary btn-lg" type="button">Kapcsolat</button>
+                                        </div>
+                                </div>
+                            ))
+                        }
                     </div>
-                ))
-            }
-            
+                </div>
         </div>
     )
 }
