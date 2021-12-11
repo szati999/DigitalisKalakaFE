45e2295afe280a0693b0ac6e98ef9d919834f5d7
Szatmari Csongi <szatmari.csongi@gmail.com>
2021-12-11 18:05:08 +0200

    Student reg design

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import UserTypes from "./components/UserTypes";
 import MentorRegister from "./components/MentorRegister";
+import StudentRegistration from './components/StudentRegistration'
 
 export default function App() {
 	return (
@@ -11,6 +12,7 @@ export default function App() {
 				<Route path="/mentor" element={<MentorRegister />} />
 				<Route path="/student"></Route>
 				<Route path="/" element={<UserTypes />}></Route>
+				<Route path='/student-registration' element={<StudentRegistration />} />
 			</Routes>
 		</Router>
 	);
