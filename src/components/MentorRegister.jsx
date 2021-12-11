2f86799410b7d70be723dee17b7d3d13f5f73d80
matetami <matetami96@gmail.com>
2021-12-11 17:08:22 +0200

    mentor register nearing finish

--- src/components/MentorRegister.jsx
+++ src/components/MentorRegister.jsx
@@ -1,76 +1,172 @@
 import React, { useRef } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Col, Form, Row } from "react-bootstrap";
+// import { useNavigate } from "react-router-dom";
 
 import classes from "./MentorRegister.module.css";
+import SUBJECTS_ARRAY from "../constants/subjects";
 
 const MentorRegister = () => {
-	const nameRef = useRef();
+	// let navigate = useNavigate();
+
+	const lastNameRef = useRef();
+	const firstNameRef = useRef();
+	const ageRef = useRef();
+	const emailRef = useRef();
+	const phoneRef = useRef();
+	const educationRef = useRef();
 	const subjectRef = useRef();
 	const descRef = useRef();
-	const emailRef = useRef();
-	const ageRef = useRef();
-	const phoneRef = useRef();
 
 	const handleRegisterMentor = async (event) => {
 		event.preventDefault();
 
-		const enteredName = nameRef.current.value;
-		const enteredSubjectType = subjectRef.current.value;
-		const enteredDesc = descRef.current.value;
-		const enteredEmail = emailRef.current.value;
+		const enteredLName = lastNameRef.current.value;
+		const enteredFName = firstNameRef.current.value;
 		const enteredAge = ageRef.current.value;
+		const enteredEmail = emailRef.current.value;
 		const enteredPhone = phoneRef.current.value;
-
-		console.log({
-			name: enteredName,
-			subject: enteredSubjectType,
-			description: enteredDesc,
-			email: enteredEmail,
-			age: enteredAge,
-			phone: enteredPhone,
-		});
+		const enteredEducation = educationRef.current.value;
+		const enteredSubject = subjectRef.current.value;
+		const enteredDescription = descRef.current.value;
 
 		try {
-			await fetch("http://localhost:3000/register_mentor", {
+			const resp = await fetch("http://localhost:3000/register_mentor", {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
 				},
 				body: JSON.stringify({
-					name: enteredName,
-					subject: enteredSubjectType,
-					description: enteredDesc,
-					email: enteredEmail,
+					name: enteredLName + enteredFName,
 					age: enteredAge,
+					email: enteredEmail,
 					phone: enteredPhone,
+					education: enteredEducation,
+					subject: enteredSubject,
+					description: enteredDescription,
+					pending: true,
 				}),
 			});
+			if (resp.status === 200 || resp.status === 201) {
+				console.log("Mentor registered successfully!", resp);
+			} else {
+				console.log("Error during register (from server) ", resp);
+			}
+			// navigate("/");
 		} catch (error) {
 			console.log("Error during register: ", error);
 		}
 	};
 
+	const renderOptions = () => {
+		return SUBJECTS_ARRAY.map((item, index) => {
+			return (
+				<option key={index} value={item}>
+					{item}
+				</option>
+			);
+		});
+	};
+
 	return (
 		<div className={classes.container}>
-			<h2 className={classes.title}>Mentor Regisztráció</h2>
+			<div>
+				<h1 className={classes.title}>Kérjük töltsd ki az alábbi űrlapot</h1>
+				<p className={classes.subTitleText}>
+					Regisztrációdhoz szükséges megadnod az alábbi adatokat, majd azok
+					egyéni elbírálása után beléphetsz a felületre.
+				</p>
+			</div>
 			<Form onSubmit={handleRegisterMentor}>
+				<h2 className={classes.h2}>Személyes adatok</h2>
+
+				<Row className="mb-3">
+					<Form.Group as={Col}>
+						<Form.Label className={classes.label}>Vezetéknév</Form.Label>
+						<Form.Control
+							type="text"
+							ref={lastNameRef}
+							placeholder="Kérjük add meg a vezetékneved"
+							required
+						/>
+					</Form.Group>
+					<Form.Group as={Col}>
+						<Form.Label className={classes.label}>Keresztnév</Form.Label>
+						<Form.Control
+							type="text"
+							ref={firstNameRef}
+							placeholder="Kérjük add meg keresztneved"
+							required
+						/>
+					</Form.Group>
+				</Row>
+				<Row>
+					<Col sm={6}>
+						<Form.Label className={classes.label}>Életkor</Form.Label>
+						<Form.Control
+							type="number"
+							ref={ageRef}
+							placeholder="Kérjük add meg az életkorod"
+							required
+						/>
+					</Col>
+				</Row>
+				<h2 className={classes.h2}>Elérhetőségi adatok</h2>
+				<Row className="mb-3">
+					<Form.Group as={Col}>
+						<Form.Label className={classes.label}>Email cím</Form.Label>
+						<Form.Control
+							type="email"
+							ref={emailRef}
+							placeholder="Kérjük add meg az email címedet"
+							required
+						/>
+					</Form.Group>
+					<Form.Group as={Col}>
+						<Form.Label className={classes.label}>Telefonszám</Form.Label>
+						<Form.Control
+							type="text"
+							ref={phoneRef}
+							placeholder="Kérjük add meg a telefonszámodat"
+							required
+						/>
+					</Form.Group>
+				</Row>
+				<h2 className={classes.h2}>Leírás</h2>
+				<Row className="mb-3">
+					<Form.Group as={Col}>
+						<Form.Label className={classes.label}>Végzettség</Form.Label>
+						<Form.Control
+							type="text"
+							ref={educationRef}
+							placeholder="Kérjük add meg az email címedet"
+							required
+						/>
+					</Form.Group>
+					<Form.Group as={Col}>
+						<Form.Label className={classes.label}>Mentor kategória</Form.Label>
+						<Form.Select ref={subjectRef} required>
+							<option value="">Kérjük válaszd ki a kategóriát</option>
+							{renderOptions()}
+						</Form.Select>
+					</Form.Group>
+				</Row>
 				<Form.Group className="mb-3">
-					<Form.Label>Név</Form.Label>
-					<Form.Control type="text" ref={nameRef} required />
-					<Form.Label>Email</Form.Label>
-					<Form.Control type="email" ref={emailRef} required />
-					<Form.Label>Szakterület</Form.Label>
-					<Form.Control type="text" ref={subjectRef} required />
-					<Form.Label>Telefonszám</Form.Label>
-					<Form.Control type="text" ref={phoneRef} required />
-					<Form.Label>Életkor</Form.Label>
-					<Form.Control type="number" ref={ageRef} />
-					<Form.Label>Leírás</Form.Label>
-					<Form.Control as="textarea" ref={descRef} />
+					<Form.Label className={classes.label}>Leírás</Form.Label>
+					<Form.Control
+						as="textarea"
+						ref={descRef}
+						placeholder="Kérjük ismertesd magad pár sorban"
+					/>
+					<p className={classes.descText}>
+						Miért szeretnél önkéntes mentor lenni? Miért gondolod, hogy
+						megfelelő vagy ehhez a feladathoz?
+					</p>
 				</Form.Group>
-				<Button variant="primary" type="submit">
-					Regisztráció
-				</Button>
+				<div className={`mb-5 ${classes.submitBtnRow}`}>
+					<Button variant="primary" type="submit" className={classes.submitBtn}>
+						Űrlap beküldése
+					</Button>
+				</div>
 			</Form>
 		</div>
 	);
