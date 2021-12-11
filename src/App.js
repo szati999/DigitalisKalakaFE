bb5fa130524e7abb059b8ec885b59609d5efdca1
matetami <matetami96@gmail.com>
2021-12-11 21:32:13 +0200

    .

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import UserTypes from "./components/UserTypes";
 import MentorRegister from "./components/MentorRegister";
-import StudentRegistration from './components/StudentRegistration'
+import StudentRegistration from "./components/StudentRegistration";
 import StudentList from "./components/StudentList";
 
 export default function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/mentor" element={<MentorRegister />} />
-				<Route path="/student" element={<StudentList />}></Route>
 				<Route path="/" element={<UserTypes />}></Route>
-				<Route path='/student-registration' element={<StudentRegistration />} />
+				<Route path="/mentor" element={<MentorRegister />} />
+				<Route path="/student-registration" element={<StudentRegistration />} />
+				<Route path="/student" element={<StudentList />}></Route>
 			</Routes>
 		</Router>
 	);
